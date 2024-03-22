473100eb0c427c1c291dcbe58e718ccd588b380b
Austin Zaccor <azaccor@gmail.com>
2024-03-22 09:25:29 -0700

    Update app.R

--- app.R
+++ app.R
@@ -3,29 +3,31 @@ library(sparklyr)
 library(tidyverse)
 library(dbplyr)
 library(DBI)
+library(DT)
 
 # Define the user interface
 ui <- fluidPage(
   titlePanel("NUFORC Data Viewer"),
   mainPanel(
     tableOutput("nuforcTable")
+    tableOutput("nuforcTable2")
   )
 )
 
-sc <- spark_connect(method = "databricks")
-nuforc <- tbl(sc, in_catalog("austin_zaccor", "r-shiny", "scrubbed"))
-nuforc_df <- collect(nuforc) %>% as.data.frame()
-head(nuforc_df)
-
-# Define server logic
 server <- function(input, output) {
+  # Data fetching and conversion outside of renderTable
+  sc <- spark_connect(method = "databricks")
+  nuforc <- tbl(sc, in_catalog("austin_zaccor", "r-shiny", "scrubbed"))
+  nuforc_df <- collect(nuforc) %>% as.data.frame()
+
   output$nuforcTable <- renderTable({
-    sc <- spark_connect(method = "databricks")
-    nuforc <- tbl(sc, in_catalog("austin_zaccor", "r-shiny", "scrubbed"))
-    nuforc_df <- collect(nuforc) %>% as.data.frame()
-    head(nuforc_df)
+    head(nuforc_df)  # Simply display the pre-processed data
   })
 }
+
+output$nuforcTable2 <- renderDataTable({
+    nuforc_df  # Display the full dataframe
+})
 
 # Run the application
 shinyApp(ui = ui, server = server)
